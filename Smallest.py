1854e18be02756d49efdddc3b878dd5bba35ba60
arnabroy2003 <118960641+arnabroy2003@users.noreply.github.com>
2023-03-03 21:38:06 +0530

    Create Smallest.py

---
+++ Smallest.py
@@ -0,0 +1,8 @@
+def find_smallest_number(numbers):
+    smallest = min(numbers)
+    return smallest
+
+list1 = [5, 7, 9, 14, 10, 20, 4]
+smallest_number = find_smallest_number(list1)
+
+print("The smallest number in the list is:", smallest_number)
